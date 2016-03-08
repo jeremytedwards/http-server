28f55afacc708bffd86cc6be6317604b44413d3d
Jeremy Edwards <jeremytedwards@gmail.com>
2016-03-07 21:02:13 -0800

    Wrapped server.py connection in try catch, to catch KeyboardInterrupt

--- client.py
+++ client.py
@@ -1,24 +1,24 @@
 # -*- coding: utf-8 -*-
 import socket
 
-
 test_message = 'Worst case test message!'
 
-
-def client(message=test_message, close=False):
+def client(message=test_message):
     infos = socket.getaddrinfo('127.0.0.1', 5001)
     stream_info = [i for i in infos if i[1] == socket.SOCK_STREAM][0]
 
-    client = socket.socket(*stream_info[:3])
-    client.connect(stream_info[-1])
-    client.sendall(message.encode('utf-8'))
+    client_socket = socket.socket(*stream_info[:3])
+    client_socket.connect(stream_info[-1])
+    client_socket.sendall(message.encode('utf-8'))
+
+    #waiting for response
     buffered_message = ""
     buffer_length = 4096
-    client.recv(buffer_length)
+    client_socket.recv(buffer_length)
     message_complete = False
     while not message_complete:
         print('waiting for response')
-        part = client.recv(buffer_length)
+        part = client_socket.recv(buffer_length)
         buffered_message += part.decode('utf-8')
         if len(part) < buffer_length:
             print(buffered_message)
@@ -37,5 +37,5 @@ def client(message=test_message, close=False):
 #             client(close=True)
 
 if __name__ == '__main__':
-    # main()
     client('This is like, our test')
+    # main()
--- server.py
+++ server.py
@@ -3,41 +3,36 @@
 import socket
 
 def server():
-    server_socket = socket.socket(socket.AF_INET,
-                                  socket.SOCK_STREAM,
-                                  socket.IPPROTO_TCP,)
-    print("\nserver: ", server_socket)
-    address = ('127.0.0.1', 5001)
-    server_socket.bind(address)
-    print("\nserver: ", server_socket)
-    server_socket.listen(1)
-    print("\nlistening...")
-    listening = True
+    try:
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP,)
+        print("\nserver: ", server_socket)
 
-    
-    while listening:
+        address = ('127.0.0.1', 5001)
+        server_socket.bind(address)
+        print("\nserver: ", server_socket)
+
+        server_socket.listen(1)
+        print("\nlistening...")
+
         conn, addr = server_socket.accept()
+
         # Receive the buffered message
         buffered_message = ""
         buffer_length = 4096
         message_complete = False
         while not message_complete:
-            try:
-                part = conn.recv(buffer_length)
-                buffered_message += part.decode('utf-8')
-                if len(part) == 0:
-                    listening = False
-                    break
-                else:
-                    if len(part) < buffer_length:
-                        conn.sendall(buffered_message.encode('utf-8'))
-                        print(buffered_message)
-                        message_complete = True
-            except KeyboardInterrupt:
-                conn.close()
-                print('connection closed')
-                server.close()
-                print('server closed')
+            part = conn.recv(buffer_length)
+            buffered_message += part.decode('utf-8')
+            if len(part) < buffer_length:
+                conn.sendall(buffered_message.encode('utf-8'))
+                print(buffered_message)
+                # message_complete = True
+
+    except KeyboardInterrupt:
+        conn.close()
+        print('connection closed')
+        server.close()
+        print('server closed')
 
 
 # TODO: When the Client hits Ctrl+D then the connection closes
