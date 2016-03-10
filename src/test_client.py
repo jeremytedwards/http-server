d9cb5ac03a0e9cb52fabe7e62467796b610d80e3
Jeremy Edwards <jeremytedwards@gmail.com>
2016-03-09 19:11:41 -0800

    Updated spacing in test_client.py to pass linting

--- src/test_client.py
+++ src/test_client.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import pytest
-
-
 # the server must be running for this test
+
+
 def test_client_shorter_than_buffer(capfd):
     from client import client
     client("test_string")
     out, err = capfd.readouterr()
     assert "HTTP/1.1 200 OK\r\n" in out
-
-
