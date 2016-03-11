f488a797b125f92fdb621768ee5a2d79c5387b5e
Jeremy Edwards <jeremytedwards@gmail.com>
2016-03-11 12:46:14 -0800

    Updated client to make requests on 5000 Updated test_client.py to send tests.

--- src/test_client.py
+++ src/test_client.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
-
+from __future__ import unicode_literals
 import pytest
-# the server must be running for this test
+
+# the server must be running for these tests
+
+CLIENT_MESSAGES = [
+    (u"GET / HTTP/1.1\r\nHost: localhost:5000\r\n", u"test string"),
+    (u'SET / HTTP/1.1\r\nHost: localhost:5000\r\n', u''),
+    (u'DELETE / HTTP/1.1\r\nHost: localhost:5000\r\n', u''),
+    (u'UPDATE / HTTP/1.1\r\nHost: localhost:5000\r\n', u''),
+]
 
 
-# CLIENT_MESSAGES = [
-# (u'GET / HTTP/1.1\r\nHost: localhost:5000\r\n', u'test string'),
-# (u'SET / HTTP/1.1\r\nHost: localhost:5000\r\n', u''),
-# (u'DELETE / HTTP/1.1\r\nHost: localhost:5000\r\n', u''),
-# (u'UPDATE / HTTP/1.1\r\nHost: localhost:5000\r\n', u'')
-# ]
-#
-#
-# @pytest.mark.parametrize('request , response', CLIENT_MESSAGES)
-# def test_client_request(request, response):
-#     from client import client
-#     assert client(request) == response
+@pytest.mark.parametrize("req, resp", CLIENT_MESSAGES)
+def test_client_request(req, resp):
+    from client import client
+    assert client(req) == resp
